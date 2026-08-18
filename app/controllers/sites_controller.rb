dfd95771fc783edd19000d51adb7e285f6b9958f
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-08-18 13:12:26 -0300

    feat: add sites CRUD

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,47 @@
+class SitesController < ApplicationController
+  before_action :set_site, only: %i[show edit update destroy]
+
+  def index
+    @sites = Site.order(:name)
+  end
+
+  def show; end
+
+  def new
+    @site = Site.new
+  end
+
+  def edit; end
+
+  def create
+    @site = Site.new(site_params)
+    if @site.save
+      redirect_to sites_path, notice: "Site criado com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @site.update(site_params)
+      redirect_to sites_path, notice: "Site atualizado com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @site.destroy!
+    redirect_to sites_path, notice: "Site removido."
+  end
+
+  private
+
+  def set_site
+    @site = Site.find(params[:id])
+  end
+
+  def site_params
+    params.require(:site).permit(:name, :domain, :active)
+  end
+end
