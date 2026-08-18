e37c5b8faa84d689f906ccc6bb63c956c1fcc3e8
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-08-18 13:12:33 -0300

    feat: add categories CRUD

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,45 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[edit update destroy]
+
+  def index
+    @categories = Category.includes(:site).order("sites.name", :name)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit; end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: "Categoria criada com sucesso."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: "Categoria atualizada com sucesso."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy!
+    redirect_to categories_path, notice: "Categoria removida."
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:site_id, :name, :slug)
+  end
+end
