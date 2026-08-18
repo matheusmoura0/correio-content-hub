700ba08f4b9d54e041b5acc674dcd6f6fb4bfd12
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-08-18 13:10:36 -0300

    feat: add MVP routes

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root "dashboard#index"
+
+  resources :sites
+  resources :categories
+  resources :feeds do
+    post :import, on: :member, action: :import_feed
+  end
+  resources :articles, only: %i[index show update]
+
+  namespace :api do
+    namespace :v1 do
+      get "sites/:site_id/articles", to: "articles#index"
+      get "articles/:id", to: "articles#show"
+    end
+  end
+end
