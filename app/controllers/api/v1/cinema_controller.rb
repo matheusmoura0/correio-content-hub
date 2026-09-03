6da7bd6b81f0fd7cdf5220644782a80b00021664
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-09-03 17:02:34 -0300

    Conecta sites reais ao Content Hub

--- app/controllers/api/v1/cinema_controller.rb
+++ app/controllers/api/v1/cinema_controller.rb
@@ -2,6 +2,7 @@ module Api
   module V1
     class CinemaController < ActionController::API
       after_action :disable_cache
+      after_action :allow_publication_sites
 
       def home
         render json: Cinema::TmdbClient.new.home
@@ -35,6 +36,15 @@ module Api
 
       def disable_cache
         response.set_header("Cache-Control", "public, max-age=60, s-maxage=600, stale-while-revalidate=3600")
+      end
+
+      def allow_publication_sites
+        allowed = Site.where(active: true).pluck(:allowed_origins).flat_map do |origins|
+          origins.to_s.lines.map(&:strip)
+        end.reject(&:blank?).uniq
+        origin = request.headers["Origin"]
+        response.set_header("Access-Control-Allow-Origin", origin) if allowed.include?(origin)
+        response.set_header("Vary", "Origin")
       end
     end
   end
