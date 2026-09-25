84d14f45f7f30449bcfe6c6fafe485db4252ad21
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-09-25 07:43:41 -0300

    Fix site associations for Barra profile

--- app/models/site.rb
+++ app/models/site.rb
@@ -25,7 +25,6 @@ class Site < ApplicationRecord
   has_many :feeds, dependent: :nullify
   has_many :site_articles, dependent: :destroy
   has_many :analytics_events, dependent: :destroy
-  has_many :sites, through: :site_articles
   has_many :articles, through: :site_articles
 
   validates :name, :domain, :publication_key, presence: true
